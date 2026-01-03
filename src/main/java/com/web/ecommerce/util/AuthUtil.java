774248705848b792612b4cf7fded95578589dd1d
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-01-04 02:58:55 +0300

    i added authUtil to gets the currently authenticated user

---
+++ src/main/java/com/web/ecommerce/util/AuthUtil.java
@@ -0,0 +1,44 @@
+package com.web.ecommerce.util;
+
+
+import com.web.ecommerce.model.User;
+import com.web.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String loggedInEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getEmail();
+    }
+
+    public Long loggedInUserId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+
+        return user.getUserId();
+    }
+
+    public User loggedInUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + authentication.getName()));
+        return user;
+
+    }
+
+
+}
