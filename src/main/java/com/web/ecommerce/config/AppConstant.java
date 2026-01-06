9b5f3325fb0237f8301b7a99fd05ee70b3246a4e
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-01-06 20:46:05 +0300

    pagination constant

---
+++ src/main/java/com/web/ecommerce/config/AppConstant.java
@@ -0,0 +1,9 @@
+package com.web.ecommerce.config;
+
+public class AppConstant {
+    public static final String DEFAULT_PAGE_SIZE = "50";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_SORT_ORDER = "asc";
+    public static final String SORT_CATEGORY_BY = "categoryId";
+    public static final String SORT_PRODUCT_BY = "productId";
+}
